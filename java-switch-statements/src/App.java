6ad6eb77084761351a266fe60f47de1f1b8d0172
github username <um_2003_8@hotmail.com>
2022-06-22 12:40:17 -0400

    create an input

--- java-switch-statements/src/App.java
+++ java-switch-statements/src/App.java
@@ -1,8 +1,15 @@
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-
+		
+			Scanner input = new Scanner(System.in);
+			// Variable types supported by switch statements: char, byte, short, int and String
+			
+			System.out.println("Enter your instructions: ");
+			String text = input.nextLine();			
 	}
 
 }
